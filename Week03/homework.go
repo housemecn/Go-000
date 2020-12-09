2fc61df77363576a911e6404e4916f5510a46410
houseme <houseme@outlook.com>
2020-12-09 21:57:39 +0800

    week03

--- Week03/homework.go
+++ Week03/homework.go
@@ -20,12 +20,14 @@ import (
 
 var ossi chan os.Signal
 
+// server http.Server
 type server struct {
 	*http.Server
 
 	name string
 }
 
+// handler handler http
 type handler struct {
 	name string
 }
@@ -36,6 +38,7 @@ func main() {
 
 	s1 := newServer("1", ":8088")
 	s2 := newServer("2", ":9099")
+	s3 := newServer("2", ":9098")
 
 	group.Go(
 		func() error {
@@ -55,8 +58,17 @@ func main() {
 		},
 	)
 
+	group.Go(
+		func() error {
+			if err := s3.ListenAndServe(); err != http.ErrServerClosed {
+				return err
+			}
+			return nil
+		},
+	)
+
 	group.Go(func() error {
-		return listenSignal(ctx, s1, s2)
+		return listenSignal(ctx, s1, s2, s3)
 	})
 
 	if err := group.Wait(); err != nil {
@@ -65,10 +77,12 @@ func main() {
 
 }
 
+// ServeHTTP Serve HTTP
 func (h handler) ServeHTTP(w http.ResponseWriter, _ *http.Request) {
 	fmt.Fprintf(w, "Server %s received request", h.name)
 }
 
+//  newServer new Server
 func newServer(name, addr string) server {
 	h := handler{name}
 
@@ -81,17 +95,21 @@ func newServer(name, addr string) server {
 	}
 }
 
-func listenSignal(ctx context.Context, s1, s2 server) error {
+// listenSignal listen Signal
+func listenSignal(ctx context.Context, s1, s2, s3 server) error {
 	signal.Notify(ossi)
 	select {
 	case sig := <-ossi:
 		fmt.Printf("Recevied start signal %s\n", sig.String())
 		// use context.Backgroud to shutdown the server because original ctx is canceled
 		if err := s1.Shutdown(context.Background()); err != nil {
-			fmt.Println("2", err)
+			fmt.Println("22", err)
 		}
 		if err := s2.Shutdown(context.Background()); err != nil {
-			fmt.Println("3", err)
+			fmt.Println("33", err)
+		}
+		if err := s3.Shutdown(context.Background()); err != nil {
+			fmt.Println("44", err)
 		}
 		return fmt.Errorf("Recevied signal %s", sig.String())
 	case <-ctx.Done():
